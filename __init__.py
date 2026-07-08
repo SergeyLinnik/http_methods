231e372264a18ab325a1b6595cbb1eb7ea41191b
Сергей <linnik_sergey@mail.ru>
2026-07-08 07:22:44 +0300

    Fix: исправлены импорты в тестах (src.http_methods)

--- __init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Package for HTTP methods.
-"""
-
-from .http_methods import HTTPMethods
-
-__all__ = ["HTTPMethods"]
--- tests/test_http_methods.py
+++ tests/test_http_methods.py
@@ -8,7 +8,7 @@ import os
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from http_methods import HTTPMethods
+from src.http_methods import HTTPMethods
 
 
 def test_get_method() -> None:
